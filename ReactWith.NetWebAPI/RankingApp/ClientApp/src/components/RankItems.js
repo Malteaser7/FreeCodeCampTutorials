9b10db1d0dfa2be851b19bb647e6637932d29766
SamIge7 <sige@spartaglobal.com>
2023-01-23 21:39:03 +0000

    Upload

--- ReactWith.NetWebAPI/RankingApp/ClientApp/src/components/RankItems.js
+++ ReactWith.NetWebAPI/RankingApp/ClientApp/src/components/RankItems.js
@@ -1,5 +1,6 @@
 ﻿import React, { useState, useEffect } from 'react';
 import MovieImageArr from "./MovieImages.js";
+import RankingGrid from "./RankingGrid.js";
 
 const RankItems = () => {
 
@@ -18,11 +19,13 @@ const RankItems = () => {
 
     return (
         <main>
+            <RankingGrid items={items} imgArr={MovieImageArr} />
             <div className="items-not-ranked">
                 {
                     (items.length > 0) ? items.map((item) =>
-
-                        <img id={`item-${item.id}`} src={MovieImageArr.find(o => o.id === item.imageId)} />
+                        <div className="unranked-cell">
+                            <img id={`item-${item.id}`} src={MovieImageArr.find(o => o.id === item.imageId)?.image} />
+                        </div>
 
                     ) : <div>Loading...</div>
                 }
